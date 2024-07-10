85afbeebb5dde2732ae8e58e8cacc49e3e60f4d0
tlm920 <timo@timomcgrath.com>
2024-07-10 12:43:51 -0400

    java 17 support - velocity.

--- common/src/main/java/com.timomcgrath.packstacker/PackStackerUtil.java
+++ common/src/main/java/com.timomcgrath.packstacker/PackStackerUtil.java
@@ -65,7 +65,7 @@ public class PackStackerUtil {
             return;
 
         packs.forEach(pack -> packInfos.add(pack.getPackInfo()));
-        AbstractResourcePack first = packs.getFirst();
+        AbstractResourcePack first = packs.get(0);
 
         ResourcePackRequest request = ResourcePackRequest.resourcePackRequest()
                 .packs(packInfos).prompt(first.getPrompt())
@@ -93,7 +93,7 @@ public class PackStackerUtil {
      */
     public static AbstractResourcePack mostPriority(List<AbstractResourcePack> packs) {
         packs.sort(new PackComparator());
-        return packs.getFirst();
+        return packs.get(0);
     }
 
     public static class PackComparator implements Comparator<AbstractResourcePack> {
